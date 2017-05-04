f74eed7fdd2c12426b7ac1b67e6b8769d78c879c
Юлия Шпак <shpa4ok@mail.ru>
2017-05-04 10:48:09 +0300

    delete mvc

--- TestProject/Global.asax.cs
+++ TestProject/Global.asax.cs
@@ -18,9 +18,9 @@ namespace TestProject
             Database.SetInitializer(new UserInitializer());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+           // FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+           // RouteConfig.RegisterRoutes(RouteTable.Routes);
+           // BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
